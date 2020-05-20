457d131c89d9aca84f708820a252c0d9a127daf7
haitani <haitani@tam-tam.co.jp>
2020-05-20 11:36:11 +0900

    Add: Dateオブジェクト用ライブラリ date-fns追加

--- app/pages/analytics.tsx
+++ app/pages/analytics.tsx
@@ -1,8 +1,11 @@
 import styles from '../style/module/analytics.module.scss';
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
 
 function Research() {
   return (
     <div className={`${styles['p-analytics']} l-main`}>
+      <p>{format(new Date(), 'MMMdo(EEEEE) a', { locale: ja })}</p>
       <p>チャートや損益</p>
     </div>
   );
--- app/pages/research.tsx
+++ app/pages/research.tsx
@@ -1,8 +1,11 @@
 import styles from '../style/module/research.module.scss';
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
 
 function Research() {
   return (
     <div className={`${styles['p-research']} l-main`}>
+      <p>{format(new Date(), 'MMMdo(EEEEE) a', { locale: ja })}</p>
       <p>ここでみんなのカブ価がみられる</p>
     </div>
   );
