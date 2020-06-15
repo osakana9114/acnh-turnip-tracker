b280a12993ee2aa33bb5f57ea2c72fd2aaab044a
haitani <haitani@tam-tam.co.jp>
2020-06-15 18:03:16 +0900

    Fix: データの受け渡しは `payload` という名前で行うようなので、名称を修正

--- app/store/stock/action.ts
+++ app/store/stock/action.ts
@@ -7,13 +7,13 @@ export const updateStock = e => dispatch => {
   e.preventDefault();
   return dispatch({
     type: stockActionTypes.UPDATE,
-    form: e.target.price,
+    payload: e.target.price,
   });
 };
 
 export const calculateStock = result => dispatch => {
   return dispatch({
     type: stockActionTypes.CALCULATE,
-    data: result,
+    payload: result,
   });
 };
--- app/store/stock/reducer.ts
+++ app/store/stock/reducer.ts
@@ -9,13 +9,15 @@ export default function reducer(state = stockInitialState, action) {
   switch (action.type) {
     case stockActionTypes.UPDATE:
       const priceValues: number[] = [];
-      [].slice.call(action.form).map(input => priceValues.push(input.value ? +input.value : null));
+      [].slice
+        .call(action.payload)
+        .map(input => priceValues.push(input.value ? +input.value : null));
       return Object.assign({}, state, {
         price: priceValues,
       });
     case stockActionTypes.CALCULATE:
       return Object.assign({}, state, {
-        calc: action.data,
+        calc: action.payload,
       });
     default:
       return state;
